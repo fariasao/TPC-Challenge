e10bebf7b424274f8a92c3b3c7e990f35ba03248
fariasao <enzofaarias2010@gmail.com>
2024-04-07 18:51:34 -0300

    Add NotificacoesRepository interface

---
+++ src/main/java/com/TPC/TPC/repository/NotificacoesRepository.java
@@ -0,0 +1,6 @@
+package com.TPC.TPC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.TPC.TPC.model.Notificacoes;
+public interface NotificacoesRepository extends JpaRepository<Notificacoes, Integer> {}

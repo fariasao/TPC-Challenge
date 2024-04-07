84e2b3c50491d4adabbe107edabd8697de3f3f94
fariasao <enzofaarias2010@gmail.com>
2024-04-07 18:50:16 -0300

    feat: Add repository Campanhas

---
+++ src/main/java/com/TPC/TPC/repository/CampanhasRepository.java
@@ -0,0 +1,6 @@
+package com.TPC.TPC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.TPC.TPC.model.Campanhas;
+public interface CampanhasRepository extends JpaRepository<Campanhas, Integer> {}

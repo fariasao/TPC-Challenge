e9ef026184b69361364e388201925293f4fe2836
fariasao <enzofaarias2010@gmail.com>
2024-04-07 18:50:25 -0300

    feat: Add repository Categoria

---
+++ src/main/java/com/TPC/TPC/repository/CategoriasRepository.java
@@ -0,0 +1,6 @@
+package com.TPC.TPC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.TPC.TPC.model.Categorias;
+public interface CategoriasRepository extends JpaRepository<Categorias, Integer> {}

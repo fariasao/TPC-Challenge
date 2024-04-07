cce444f780f60b3fc2538028b70f4fe012602284
fariasao <enzofaarias2010@gmail.com>
2024-04-07 19:30:53 -0300

    Add @Data annotation to Credit class

--- src/main/java/com/TPC/TPC/model/Credit.java
+++ src/main/java/com/TPC/TPC/model/Credit.java
@@ -4,18 +4,22 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "tb_credit")
 public class Credit {
     
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "creditid")
     private Integer creditId;
 

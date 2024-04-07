f37b6fc242d0a4e10def6b2beb44f3be06ed52e0
fariasao <enzofaarias2010@gmail.com>
2024-04-07 18:51:02 -0300

    Add ComprasRepository interface

---
+++ src/main/java/com/TPC/TPC/repository/ComprasRepository.java
@@ -0,0 +1,6 @@
+package com.TPC.TPC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.TPC.TPC.model.Compras;
+public interface ComprasRepository extends JpaRepository<Compras, Integer> {}

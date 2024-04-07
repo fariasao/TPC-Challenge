2059b2289976b304276845165d2d2f1f973fa975
fariasao <enzofaarias2010@gmail.com>
2024-04-07 15:48:56 -0300

    Start project

---
+++ src/main/java/com/TPC/TPC/TpcApplication.java
@@ -0,0 +1,13 @@
+package com.TPC.TPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpcApplication.class, args);
+	}
+
+}

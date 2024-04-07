eadcf70296bac19e87b8e8010aa610a438ed6861
fariasao <enzofaarias2010@gmail.com>
2024-04-07 19:40:13 -0300

    Add annotations and lombok to UserCluster model class

--- src/main/java/com/TPC/TPC/model/UserCluster.java
+++ src/main/java/com/TPC/TPC/model/UserCluster.java
@@ -2,15 +2,19 @@ package com.TPC.TPC.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Entity
+@Data 
 @Table(name = "tb_usercluster")
 public class UserCluster {
     
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "clusterinfoid")
     private Integer clusterInfoId;
 

cb7a13ba40f3508364bc71f4258af92a33a07c1d
fariasao <enzofaarias2010@gmail.com>
2024-04-07 19:24:22 -0300

    Add annotations and lombok to CompraPontos class

--- src/main/java/com/TPC/TPC/model/CompraPontos.java
+++ src/main/java/com/TPC/TPC/model/CompraPontos.java
@@ -2,17 +2,21 @@ package com.TPC.TPC.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "tb_comprapontos")
 public class CompraPontos {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "pedidoid")
     private Integer pedidoId;
 
